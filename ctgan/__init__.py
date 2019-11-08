a63581522b63d294ee83401e736768b121a26812
Carles Sala <carles@pythiac.com>
2019-11-07 22:16:14 -0500

    Bump version: 0.1.0 → 0.1.1.dev0

--- ctgan/__init__.py
+++ ctgan/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1.dev0'
 
 from ctgan.model import CTGANSynthesizer
 
--- setup.py
+++ setup.py
@@ -95,6 +95,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/DAI-Lab/CTGAN',
-    version='0.1.0',
+    version='0.1.1.dev0',
     zip_safe=False,
 )
