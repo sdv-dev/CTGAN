a1125f8a6b69ad7cddfb948e67aed90f9f9aa7a0
Carles Sala <carles@pythiac.com>
2019-12-18 18:37:26 +0100

    Bump version: 0.2.0.dev0 → 0.2.0.dev1

--- setup.py
+++ setup.py
@@ -94,6 +94,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/DAI-Lab/CTGAN',
-    version='0.2.0.dev0',
+    version='0.2.0.dev1',
     zip_safe=False,
 )
