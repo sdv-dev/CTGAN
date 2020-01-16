038662c785e0ffca971ae9a0ae3c77a6b4ce47d1
kevinykuo <kevinykuo@gmail.com>
2020-01-16 09:04:51 -0800

    don't bother mocking

--- tests/integration/test_ctgan.py
+++ tests/integration/test_ctgan.py
@@ -9,8 +9,6 @@ but correctness of the data values and the internal behavior of the
 model are not checked.
 """
 
-from unittest.mock import patch
-
 import numpy as np
 import pandas as pd
 
@@ -53,12 +51,11 @@ def test_ctgan_numpy():
 
 
 def test_log_frequency():
-    with patch('numpy.random.choice',
-               return_value=np.repeat(['a', 'b', 'c'], [950, 25, 25])):
-        data = pd.DataFrame({
-            'continuous': np.random.random(1000),
-            'discrete': np.random.choice(['a', 'b', 'c'], 1000, p=[0.95, 0.025, 0.025])
-        })
+
+    data = pd.DataFrame({
+        'continuous': np.random.random(1000),
+        'discrete': np.repeat(['a', 'b', 'c'], [950, 25, 25])
+    })
 
     discrete_columns = ['discrete']
 
