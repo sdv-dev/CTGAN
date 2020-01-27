b0861bed14cf62c4d12b2273a18e5f4b98f24c89
Carles Sala <carles@pythiac.com>
2020-01-27 08:03:30 -0500

    Bump version: 0.2.1 → 0.2.2.dev0

--- ctgan/__init__.py
+++ ctgan/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2.dev0'
 
 from ctgan.demo import load_demo
 from ctgan.synthesizer import CTGANSynthesizer
--- setup.py
+++ setup.py
@@ -94,6 +94,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/CTGAN',
-    version='0.2.1',
+    version='0.2.2.dev0',
     zip_safe=False,
 )
