4112c9483b136db5508dc0cf38807fd76ad41387
Lei Xu <leix@mit.edu>
2020-06-17 00:48:22 -0400

    change code to 2 space indent

--- ctgan/data_sampler.py
+++ ctgan/data_sampler.py
@@ -2,101 +2,100 @@ import numpy as np
 
 
 class DataSampler(object):
-    def __init__(self, data, output_info, log_frequency):
-        self.data = data
-        self.model = []
-        self.model2 = []
-        start = 0
-        skip = False
-        max_interval = 0
-        counter = 0
-        for items in output_info:
-            if len(items) == 2:
-                start += items[0][0] + items[1][0]
-                continue
-            else:
-                assert len(items) == 1
-                item = items[0]
-                end = start + item[0]
-                max_interval = max(max_interval, end - start)
-                counter += 1
-                self.model.append(np.argmax(data[:, start:end], axis=-1))
+  def __init__(self, data, output_info, log_frequency):
+    self.data = data
+    self.model = []
+    self.model2 = []
+    start = 0
+    skip = False
+    max_interval = 0
+    counter = 0
+    for items in output_info:
+      if len(items) == 2:
+        start += items[0][0] + items[1][0]
+        continue
+      else:
+        assert len(items) == 1
+        item = items[0]
+        end = start + item[0]
+        max_interval = max(max_interval, end - start)
+        counter += 1
+        self.model.append(np.argmax(data[:, start:end], axis=-1))
 
-                tmp = []
-                for j in range(item[0]):
-                    tmp.append(np.nonzero(data[:, start + j])[0])
+        tmp = []
+        for j in range(item[0]):
+          tmp.append(np.nonzero(data[:, start + j])[0])
 
-                self.model2.append(tmp)
-                start = end
-        assert start == data.shape[1]
+        self.model2.append(tmp)
+        start = end
+    assert start == data.shape[1]
 
-        self.interval = []
-        self.n_col = 0
-        self.n_opt = 0
-        start = 0
-        self.p = np.zeros((counter, max_interval))
-        for items in output_info:
-            if len(items) == 2:
-                start += items[0][0] + items[1][0]
-                continue
-            else:
-                assert len(items) == 1
-                item = items[0]
-                end = start + item[0]
-                tmp = np.sum(data[:, start:end], axis=0)
-                if log_frequency:
-                    tmp = np.log(tmp + 1)
-                tmp = tmp / np.sum(tmp)
-                self.p[self.n_col, :item[0]] = tmp
-                self.interval.append((self.n_opt, item[0]))
-                self.n_opt += item[0]
-                self.n_col += 1
-                start = end
+    self.interval = []
+    self.n_col = 0
+    self.n_opt = 0
+    start = 0
+    self.p = np.zeros((counter, max_interval))
+    for items in output_info:
+      if len(items) == 2:
+        start += items[0][0] + items[1][0]
+        continue
+      else:
+        assert len(items) == 1
+        item = items[0]
+        end = start + item[0]
+        tmp = np.sum(data[:, start:end], axis=0)
+        if log_frequency:
+          tmp = np.log(tmp + 1)
+        tmp = tmp / np.sum(tmp)
+        self.p[self.n_col, :item[0]] = tmp
+        self.interval.append((self.n_opt, item[0]))
+        self.n_opt += item[0]
+        self.n_col += 1
+        start = end
 
+    self.interval = np.asarray(self.interval)
 
-        self.interval = np.asarray(self.interval)
+  def random_choice_prob_index(self, idx):
+    a = self.p[idx]
+    r = np.expand_dims(np.random.rand(a.shape[0]), axis=1)
+    return (a.cumsum(axis=1) > r).argmax(axis=1)
 
-    def random_choice_prob_index(self, idx):
-        a = self.p[idx]
-        r = np.expand_dims(np.random.rand(a.shape[0]), axis=1)
-        return (a.cumsum(axis=1) > r).argmax(axis=1)
+  def sample_condvec(self, batch):
+    if self.n_col == 0:
+      return None
 
-    def sample_condvec(self, batch):
-        if self.n_col == 0:
-            return None
+    batch = batch
+    idx = np.random.choice(np.arange(self.n_col), batch)
 
-        batch = batch
-        idx = np.random.choice(np.arange(self.n_col), batch)
+    vec1 = np.zeros((batch, self.n_opt), dtype='float32')
+    mask1 = np.zeros((batch, self.n_col), dtype='float32')
+    mask1[np.arange(batch), idx] = 1
+    opt1prime = self.random_choice_prob_index(idx)
+    opt1 = self.interval[idx, 0] + opt1prime
+    vec1[np.arange(batch), opt1] = 1
 
-        vec1 = np.zeros((batch, self.n_opt), dtype='float32')
-        mask1 = np.zeros((batch, self.n_col), dtype='float32')
-        mask1[np.arange(batch), idx] = 1
-        opt1prime = self.random_choice_prob_index(idx)
-        opt1 = self.interval[idx, 0] + opt1prime
-        vec1[np.arange(batch), opt1] = 1
+    return vec1, mask1, idx, opt1prime
 
-        return vec1, mask1, idx, opt1prime
+  def sample_zero_condvec(self, batch):
+    if self.n_col == 0:
+      return None
 
-    def sample_zero_condvec(self, batch):
-        if self.n_col == 0:
-            return None
+    vec = np.zeros((batch, self.n_opt), dtype='float32')
+    idx = np.random.choice(np.arange(self.n_col), batch)
+    for i in range(batch):
+      col = idx[i]
+      pick = int(np.random.choice(self.model[col]))
+      vec[i, pick + self.interval[col, 0]] = 1
 
-        vec = np.zeros((batch, self.n_opt), dtype='float32')
-        idx = np.random.choice(np.arange(self.n_col), batch)
-        for i in range(batch):
-            col = idx[i]
-            pick = int(np.random.choice(self.model[col]))
-            vec[i, pick + self.interval[col, 0]] = 1
+    return vec
 
-        return vec
+  def sample(self, n, col, opt):
+    if col is None:
+      idx = np.random.choice(np.arange(self.n), n)
+      return self.data[idx]
 
-    def sample(self, n, col, opt):
-        if col is None:
-            idx = np.random.choice(np.arange(self.n), n)
-            return self.data[idx]
+    idx = []
+    for c, o in zip(col, opt):
+      idx.append(np.random.choice(self.model2[c][o]))
 
-        idx = []
-        for c, o in zip(col, opt):
-            idx.append(np.random.choice(self.model2[c][o]))
-
-        return self.data[idx]
+    return self.data[idx]
--- tests/integration/test_ctgan.py
+++ tests/integration/test_ctgan.py
@@ -10,65 +10,65 @@ model are not checked.
 """
 
 import numpy as np
-import pandas as pd
 
+import pandas as pd
 from ctgan.synthesizer import CTGANSynthesizer
 
 
 def test_ctgan_dataframe():
-    data = pd.DataFrame({
-        'continuous': np.random.random(100),
-        'discrete': np.random.choice(['a', 'b', 'c'], 100)
-    })
-    discrete_columns = ['discrete']
+  data = pd.DataFrame({
+      'continuous': np.random.random(100),
+      'discrete': np.random.choice(['a', 'b', 'c'], 100)
+  })
+  discrete_columns = ['discrete']
 
-    ctgan = CTGANSynthesizer()
-    ctgan.fit(data, discrete_columns, epochs=1)
+  ctgan = CTGANSynthesizer()
+  ctgan.fit(data, discrete_columns, epochs=1)
 
-    sampled = ctgan.sample(100)
+  sampled = ctgan.sample(100)
 
-    assert sampled.shape == (100, 2)
-    assert isinstance(sampled, pd.DataFrame)
-    assert set(sampled.columns) == {'continuous', 'discrete'}
-    assert set(sampled['discrete'].unique()) == {'a', 'b', 'c'}
+  assert sampled.shape == (100, 2)
+  assert isinstance(sampled, pd.DataFrame)
+  assert set(sampled.columns) == {'continuous', 'discrete'}
+  assert set(sampled['discrete'].unique()) == {'a', 'b', 'c'}
 
 
 def test_ctgan_numpy():
-    data = pd.DataFrame({
-        'continuous': np.random.random(100),
-        'discrete': np.random.choice(['a', 'b', 'c'], 100)
-    })
-    discrete_columns = [1]
+  data = pd.DataFrame({
+      'continuous': np.random.random(100),
+      'discrete': np.random.choice(['a', 'b', 'c'], 100)
+  })
+  discrete_columns = [1]
 
-    ctgan = CTGANSynthesizer()
-    ctgan.fit(data.values, discrete_columns, epochs=1)
+  ctgan = CTGANSynthesizer()
+  ctgan.fit(data.values, discrete_columns, epochs=1)
 
-    sampled = ctgan.sample(100)
+  sampled = ctgan.sample(100)
 
-    assert sampled.shape == (100, 2)
-    assert isinstance(sampled, np.ndarray)
-    assert set(np.unique(sampled[:, 1])) == {'a', 'b', 'c'}
+  assert sampled.shape == (100, 2)
+  assert isinstance(sampled, np.ndarray)
+  assert set(np.unique(sampled[:, 1])) == {'a', 'b', 'c'}
 
 
 def test_log_frequency():
 
-    data = pd.DataFrame({
-        'continuous': np.random.random(1000),
-        'discrete': np.repeat(['a', 'b', 'c'], [950, 25, 25])
-    })
+  data = pd.DataFrame({
+      'continuous': np.random.random(1000),
+      'discrete': np.repeat(['a', 'b', 'c'], [950, 25, 25])
+  })
 
-    discrete_columns = ['discrete']
+  discrete_columns = ['discrete']
 
-    ctgan = CTGANSynthesizer()
-    ctgan.fit(data, discrete_columns, epochs=100)
+  ctgan = CTGANSynthesizer()
+  ctgan.fit(data, discrete_columns, epochs=100)
 
-    sampled = ctgan.sample(10000)
-    counts = sampled['discrete'].value_counts()
-    assert counts['a'] < 6500
+  sampled = ctgan.sample(10000)
+  counts = sampled['discrete'].value_counts()
+  assert counts['a'] < 6500
 
-    ctgan = CTGANSynthesizer()
-    ctgan.fit(data, discrete_columns, epochs=100, log_frequency=False)
+  ctgan = CTGANSynthesizer()
+  ctgan.fit(data, discrete_columns, epochs=100, log_frequency=False)
 
-    sampled = ctgan.sample(10000)
-    counts = sampled['discrete'].value_counts()
-    assert counts['a'] > 9000
+  sampled = ctgan.sample(10000)
+  counts = sampled['discrete'].value_counts()
+  assert counts['a'] > 9000
