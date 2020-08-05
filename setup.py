13df633229cbc9d85f62fe0396aaa535e933b459
Carles Sala <carles@pythiac.com>
2020-08-05 17:02:16 +0200

    Bump version: 0.2.2.dev1 → 0.3.0.dev0

--- setup.py
+++ setup.py
@@ -95,6 +95,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/CTGAN',
-    version='0.2.2.dev1',
+    version='0.3.0.dev0',
     zip_safe=False,
 )
