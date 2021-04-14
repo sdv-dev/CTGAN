d2f8f0403a908bff35931b85dadbbf231aef75d4
Felipe Hofmann <fealho@gmail.com>
2021-04-13 20:05:27 -0700

    Fix default value

--- ctgan/synthesizers/ctgan.py
+++ ctgan/synthesizers/ctgan.py
@@ -133,7 +133,7 @@ class CTGANSynthesizer(BaseSynthesizer):
 
     def __init__(self, embedding_dim=128, generator_dim=(256, 256), discriminator_dim=(256, 256),
                  generator_lr=2e-4, generator_decay=1e-6, discriminator_lr=2e-4,
-                 discriminator_decay=0, batch_size=500, discriminator_steps=1, log_frequency=True,
+                 discriminator_decay=1e-6, batch_size=500, discriminator_steps=1, log_frequency=True,
                  verbose=False, epochs=300, pac=10, cuda=True):
 
         assert batch_size % 2 == 0
