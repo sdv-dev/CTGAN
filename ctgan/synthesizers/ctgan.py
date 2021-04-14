1b42bcfb63df5d80bbfcd501becaee70d62ec340
Felipe Hofmann <fealho@gmail.com>
2021-04-13 20:11:27 -0700

    Fix lint

--- ctgan/synthesizers/ctgan.py
+++ ctgan/synthesizers/ctgan.py
@@ -133,8 +133,8 @@ class CTGANSynthesizer(BaseSynthesizer):
 
     def __init__(self, embedding_dim=128, generator_dim=(256, 256), discriminator_dim=(256, 256),
                  generator_lr=2e-4, generator_decay=1e-6, discriminator_lr=2e-4,
-                 discriminator_decay=1e-6, batch_size=500, discriminator_steps=1, log_frequency=True,
-                 verbose=False, epochs=300, pac=10, cuda=True):
+                 discriminator_decay=1e-6, batch_size=500, discriminator_steps=1,
+                 log_frequency=True, verbose=False, epochs=300, pac=10, cuda=True):
 
         assert batch_size % 2 == 0
 
