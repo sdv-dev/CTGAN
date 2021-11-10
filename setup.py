66ab4b1bbe239ff1d04c441b71e7f1950e78d89b
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-11-10 15:51:54 +0100

    Bump version: 0.4.4.dev1 → 0.5.0.dev0

--- setup.py
+++ setup.py
@@ -92,6 +92,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/CTGAN',
-    version='0.4.4.dev1',
+    version='0.5.0.dev0',
     zip_safe=False,
 )
