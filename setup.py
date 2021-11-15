aaf6090c70c68ceb3e4db637eb575655bca77f36
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-11-16 00:10:32 +0100

    Bump packaging

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.md', encoding='utf-8') as history_file:
     history = history_file.read()
 
 install_requires = [
-    'packaging>=14,<22',
+    'packaging>=20,<22',
     "numpy>=1.18.0,<1.20.0;python_version<'3.7'",
     "numpy>=1.20.0,<2;python_version>='3.7'",
     'pandas>=1.1.3,<2',
