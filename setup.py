bf8178f4141c36be0ce768464371d0502a6cb131
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-11-15 21:10:52 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ with open('HISTORY.md', encoding='utf-8') as history_file:
     history = history_file.read()
 
 install_requires = [
-    'packaging>=21,<22',
+    'packaging',
     "numpy>=1.19.0,<1.20.0;python_version<'3.7'",
     "numpy>=1.20.0,<2;python_version>='3.7'",
     'pandas>=1.1.3,<2',
     'scikit-learn>=0.24,<2',
-    'torch>=1.4,<2',
+    'torch>=1.8.0,<2',
     'torchvision>=0.5.0,<1',
     'rdt>=0.6.1,<0.7',
 ]
