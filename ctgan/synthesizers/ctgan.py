4106411ea45ff024050e0e447ed32c492cc0e580
Felipe Alex Hofmann <fealho@gmail.com>
2021-11-30 05:28:30 -0800

    Add addon (#185)

--- ctgan/synthesizers/ctgan.py
+++ ctgan/synthesizers/ctgan.py
@@ -404,7 +404,7 @@ class CTGANSynthesizer(BaseSynthesizer):
                 optimizerG.step()
 
             if self._verbose:
-                print(f'Epoch {i+1}, Loss G: {loss_g.detach().cpu(): .4f},'
+                print(f'Epoch {i+1}, Loss G: {loss_g.detach().cpu(): .4f},'  # noqa: T001
                       f'Loss D: {loss_d.detach().cpu(): .4f}',
                       flush=True)
 
