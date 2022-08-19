a1791ad0a2cf6326161fcb9419f4f4145064ddc6
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-08-18 23:11:06 -0400

    Bump version: 0.5.2 → 0.5.3.dev0

--- setup.py
+++ setup.py
@@ -115,6 +115,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/CTGAN',
-    version='0.5.2',
+    version='0.5.3.dev0',
     zip_safe=False,
 )
