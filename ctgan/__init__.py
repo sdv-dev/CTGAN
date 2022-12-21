d6b7b3041ecd40f181158800a0112b804332c8ae
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2022-12-21 10:30:09 +0100

    Update email and author (#261)
    * Update email and author
    
    * Add LICENSE file
    
    * Delete conda folder

--- ctgan/__init__.py
+++ ctgan/__init__.py
@@ -2,8 +2,8 @@
 
 """Top-level package for ctgan."""
 
-__author__ = 'MIT Data To AI Lab'
-__email__ = 'dailabmit@gmail.com'
+__author__ = 'DataCebo, Inc.'
+__email__ = 'info@sdv.dev'
 __version__ = '0.6.1.dev0'
 
 from ctgan.demo import load_demo
