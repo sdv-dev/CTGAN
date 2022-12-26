d0e885ee4f7a16e47489364f1323ab028a6ac34d
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2022-12-26 23:20:23 +0100

    Add python 3.10 and 3.11 support and drop 3.6

--- setup.py
+++ setup.py
@@ -13,13 +13,14 @@ with open('HISTORY.md', encoding='utf-8') as history_file:
 
 install_requires = [
     'packaging>=20,<22',
-    "numpy>=1.18.0,<1.20.0;python_version<'3.7'",
-    "numpy>=1.20.0,<2;python_version>='3.7'",
-    'pandas>=1.1.3,<2',
-    'scikit-learn>=0.24,<2',
-    'torch>=1.8.0,<2',
-    'torchvision>=0.9.0,<1',
-    'rdt>=1.2.0,<2.0',
+    "numpy>=1.20.0,<2;python_version>='3.7' and python_version<'3.10'",
+    "numpy>=1.23.3,<2;python_version>='3.10'",
+    "pandas>=1.1.3,<2;python_version<'3.10'",
+    "pandas>=1.5.0,<2;python_version>='3.10'",
+    "scikit-learn>=1.1.3,<2;python_version>='3.10'",
+    "torch>=1.8.0,<2;python_version<'3.10'",
+    "torch>=1.13.0,<2;python_version>='3.10'",
+    'rdt @ git+https://github.com/sdv-dev/rdt.git@issue-593-support-python-3.10-and-3.11'
 ]
 
 setup_requires = [
@@ -86,10 +87,11 @@ setup(
         'License :: Free for non-commercial use',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
     description='Create tabular synthetic data using a conditional GAN',
@@ -111,7 +113,7 @@ setup(
     keywords='ctgan CTGAN',
     name='ctgan',
     packages=find_packages(include=['ctgan', 'ctgan.*']),
-    python_requires='>=3.6,<3.10',
+    python_requires='>=3.7,<3.12',
     setup_requires=setup_requires,
     test_suite='tests',
     tests_require=tests_require,
