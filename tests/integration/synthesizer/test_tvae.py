d1f67b6632ea19e76531c9bdabf5fe0c40d51f5c
Andrew Montanez <andrew@sdv.dev>
2023-05-03 22:11:29 -0500

    fixing fragile tests

--- tests/integration/synthesizer/test_tvae.py
+++ tests/integration/synthesizer/test_tvae.py
@@ -61,18 +61,21 @@ def test_continuous():
     """Test training the TVAE synthesizer on a small continuous dataset."""
     # verify that the distribution of the samples is close to the distribution of the data
     # using a kstest.
+    pass
 
 
 def test_categorical():
     """Test training the TVAE synthesizer on a small categorical dataset."""
     # verify that the distribution of the samples is close to the distribution of the data
     # using a cstest.
+    pass
 
 
 def test_mixed():
     """Test training the TVAE synthesizer on a small mixed-type dataset."""
     # verify that the distribution of the samples is close to the distribution of the data
     # using a kstest for continuous + a cstest for categorical.
+    pass
 
 
 def test__loss_function():
