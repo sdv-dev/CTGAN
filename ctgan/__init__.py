b42f3aef4c99e3cdfb580c0c3d0061e019f37535
Andrew Montanez <andrew@sdv.dev>
2023-05-09 12:51:59 -0500

    Bump version: 0.7.2.dev2 → 0.7.2

--- ctgan/__init__.py
+++ ctgan/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'DataCebo, Inc.'
 __email__ = 'info@sdv.dev'
-__version__ = '0.7.2.dev2'
+__version__ = '0.7.2'
 
 from ctgan.demo import load_demo
 from ctgan.synthesizers.ctgan import CTGAN
