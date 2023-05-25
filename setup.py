13f9a0ede7cfd7390ded5ee85225b45a8fd1a062
Andrew Montanez <andrew@sdv.dev>
2023-05-25 13:15:19 -0500

    Bump version: 0.7.3.dev0 → 0.7.3.dev1

--- setup.py
+++ setup.py
@@ -117,6 +117,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/CTGAN',
-    version='0.7.3.dev0',
+    version='0.7.3.dev1',
     zip_safe=False,
 )
