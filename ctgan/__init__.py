1126d75c1085b3b688ffb597042be4663f8e3473
Frances Hartwell <frances.hartwell@alteryx.com>
2024-02-13 15:14:42 -0500

    Bump version: 0.9.0.dev0 → 0.9.0.dev1

--- ctgan/__init__.py
+++ ctgan/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'DataCebo, Inc.'
 __email__ = 'info@sdv.dev'
-__version__ = '0.9.0.dev0'
+__version__ = '0.9.0.dev1'
 
 from ctgan.demo import load_demo
 from ctgan.synthesizers.ctgan import CTGAN
