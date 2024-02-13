4e79df332314d16b35f85ee61183d7ff84d50921
Frances Hartwell <frances.hartwell@alteryx.com>
2024-02-13 16:25:05 -0500

    Bump version: 0.9.0.dev1 → 0.9.0

--- setup.py
+++ setup.py
@@ -119,6 +119,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/CTGAN',
-    version='0.9.0.dev1',
+    version='0.9.0',
     zip_safe=False,
 )
